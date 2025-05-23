715450418a910a8ce644a56ea9625cd2cc52d657
Zia-11 <zakharchuk.ial@students.dvfu.ru>
2025-05-23 13:40:15 +1000

    добавление запроса post и возврат json ответа

--- лаба1/main.py
+++ лаба1/main.py
@@ -1,6 +1,7 @@
 #  вызов unicorh:  uvicorn main:app --reload
 
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Response
+from pydantic import BaseModel
 
 app = FastAPI()  # объект класса FastAPI() созданный внутри файла main
 
@@ -16,6 +17,8 @@ books = [
         "author": "Петя",
     }
 ]
+
+# GET запросы
 
 # Декоратор, указывает FastAPI, что функция под ним, отвечает за обработку запросов, поступающих по адресу
 
@@ -33,3 +36,27 @@ def get_book(book_id: int):
             return book
     # не найдено - ошибка
     raise HTTPException(status_code=404, detail='Книга не найдена')
+
+# POST запрос
+
+
+class NewBook(BaseModel):  # наследуемся от BaseModel
+    title: str
+    author: str
+
+
+@app.post("/books", tags=["Книги"], summary="Добавление книжки")
+def create_book(new_book: NewBook):  # функция на добавление книжек
+    books.append(
+        {
+            "id": len(books) + 1,
+            "title": new_book.title,
+            "author": new_book.author,
+        }
+    )
+    # отправляем сообщение если все успешно
+    return Response(
+        # Возврат простого JSON-ответа через Response
+        content='{"success": true, "message": "Книга добавлена"}',
+        media_type="application/json"
+    )
