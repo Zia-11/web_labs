8e282cad2a90494200815e445a5a397dd8e03fd4
Zia-11 <zakharchuk.ial@students.dvfu.ru>
2025-05-23 13:05:01 +1000

    добавлен второй get запрос

--- лаба1/main.py
+++ лаба1/main.py
@@ -1,10 +1,35 @@
 #  вызов unicorh:  uvicorn main:app --reload
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
-app = FastAPI() #объект класса FastAPI() созданный внутри файла main
+app = FastAPI()  # объект класса FastAPI() созданный внутри файла main
+
+books = [
+    {
+        "id": 1,
+        "title": "Асинхронность на Python",
+        "author": "Вася",
+    },
+    {
+        "id": 2,
+        "title": "Backend разработка на Python",
+        "author": "Петя",
+    }
+]
+
+# Декоратор, указывает FastAPI, что функция под ним, отвечает за обработку запросов, поступающих по адресу
 
 
-@app.get("/items/{item_id}")  #Декоратор, указывает FastAPI, что функция под ним, отвечает за обработку запросов, поступающих по адресу
-async def read_item(item_id: int): #вызывается каждый раз когда поступает запрос get
-    return {"item_id": item_id}
+@app.get("/books", tags=["Книги"], summary="Получить все книги")
+async def read_books():  # вызывается каждый раз когда поступает запрос get
+    return books
+
+
+# получение id книжек
+@app.get("/books/{book_id}", tags=["Книги"], summary="Получить конкретную книжку")
+def get_book(book_id: int):
+    for book in books:
+        if book["id"] == book_id:  # если найдено - круто
+            return book
+    # не найдено - ошибка
+    raise HTTPException(status_code=404, detail='Книга не найдена')
